16a9bcf4f0a1cb7b90299af86423a9f03c7c7d70
Christoph Reiter <reiter.christoph@gmail.com>
2017-05-01 15:41:56 +0200

    tests: add a test for sourcing a pygame image from an imagesurface buffer

---
+++ tests/test_surface_pygame.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import os
+import tempfile
+
+import cairo
+import pytest
+
+pygame = pytest.importorskip("pygame")
+
+
+def test_image_surface_to_pygame_image():
+    width, height = 100, 100
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+    buf = surface.get_data()
+    image = pygame.image.frombuffer(buf, (width, height), "ARGB")
+    assert image
