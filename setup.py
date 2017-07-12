6fa55284f74b2515f6f885dc7d4dce7f8bcc8bd7
Christoph Reiter <reiter.christoph@gmail.com>
2017-07-12 16:01:51 +0200

    release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 from distutils.core import Extension, setup, Command, Distribution
 
 
-PYCAIRO_VERSION = '1.13.4'
+PYCAIRO_VERSION = '1.14.0'
 CAIRO_VERSION_REQUIRED = '1.13.1'
 XPYB_VERSION_REQUIRED = '1.3'
 
