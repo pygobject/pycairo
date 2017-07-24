1355450a97f8cd41c3bcf81f35673382cb51754d
Christoph Reiter <reiter.christoph@gmail.com>
2017-07-24 16:05:05 +0200

    release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import errno
 from distutils.core import Extension, setup, Command, Distribution
 
 
-PYCAIRO_VERSION = '1.14.1'
+PYCAIRO_VERSION = '1.15.0'
 CAIRO_VERSION_REQUIRED = '1.13.1'
 XPYB_VERSION_REQUIRED = '1.3'
 
