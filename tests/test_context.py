817f54ffeea79666b5809117ecdb8285c7043643
Christoph Reiter <reiter.christoph@gmail.com>
2017-08-14 22:29:28 +0200

    tests: context coverage

--- tests/test_context.py
+++ tests/test_context.py
@@ -76,18 +76,24 @@ def test_append_path(context):
     context.new_path()
     context.append_path(p)
     assert str(context.copy_path()) == str(p)
+    with pytest.raises(TypeError):
+        context.append_path(object())
 
 
 def test_arc(context):
     assert not list(context.copy_path())
     context.arc(0, 0, 0, 0, 0)
     assert list(context.copy_path())
+    with pytest.raises(TypeError):
+        context.arc(object())
 
 
 def test_arc_negative(context):
     assert not list(context.copy_path())
     context.arc_negative(0, 0, 0, 0, 0)
     assert list(context.copy_path())
+    with pytest.raises(TypeError):
+        context.arc_negative(object())
 
 
 def test_clip_extents(context):
@@ -141,10 +147,37 @@ def test_set_get_dash(context):
     assert context.get_dash() == ((0.0, 1.0, 2.0, 3.0), 4.0)
     assert context.get_dash_count() == 4
 
+    with pytest.raises(TypeError):
+        context.set_dash()
+
+    with pytest.raises(TypeError):
+        context.set_dash(1, 10)
+
+    with pytest.raises(TypeError):
+        context.set_dash([object()], 1)
+
 
 def test_glyph_extents(context):
     with pytest.raises(TypeError):
         context.glyph_extents(None)
+    with pytest.raises(TypeError):
+        context.glyph_extents()
+
+
+def test_glyph_path(context):
+    with pytest.raises(TypeError):
+        context.glyph_path(None)
+    with pytest.raises(TypeError):
+        context.glyph_path()
+
+
+def test_in_stroke(context):
+    context.line_to(0, 0)
+    context.line_to(1, 1)
+    assert context.in_stroke(0, 0)
+    assert not context.in_stroke(0, 2)
+    with pytest.raises(TypeError):
+        context.in_stroke(object(), 0)
 
 
 def test_current_point(context):
@@ -201,6 +234,9 @@ def test_push_pop_group(context):
     context.push_group()
     context.pop_group_to_source()
 
+    with pytest.raises(TypeError):
+        context.push_group_with_content(object())
+
     context.push_group_with_content(cairo.Content.COLOR)
     context.pop_group()
 
@@ -211,6 +247,14 @@ def test_push_pop_group(context):
 def test_rectangle(context):
     context.rectangle(1, 2, 4, 5)
     assert context.path_extents() == (1.0, 2.0, 5.0, 7.0)
+    with pytest.raises(TypeError):
+        context.rectangle(1, 2, 3, object())
+
+
+def test_rotate(context):
+    context.rotate(0.3)
+    with pytest.raises(TypeError):
+        context.rotate(object())
 
 
 def test_rel_curve_to(context):
@@ -218,6 +262,13 @@ def test_rel_curve_to(context):
     context.rel_curve_to(0, 0, 0, 0, 0, 0)
     with pytest.raises(TypeError):
         context.rel_curve_to(object(), 0, 0, 0, 0, 0)
+
+
+def test_move_to(context):
+    context.move_to(10, 10)
+    assert context.get_current_point() == (10, 10)
+    with pytest.raises(TypeError):
+        context.rel_line_to(object(), 0)
 
 
 def test_rel_line_to(context):
@@ -254,11 +305,15 @@ def test_select_font_face(context):
 def test_set_antialias(context):
     context.set_antialias(cairo.Antialias.SUBPIXEL)
     assert context.get_antialias() == cairo.Antialias.SUBPIXEL
+    with pytest.raises(TypeError):
+        context.set_antialias(object())
 
 
 def test_set_fill_rule(context):
     context.set_fill_rule(cairo.FillRule.EVEN_ODD)
     assert context.get_fill_rule() == cairo.FillRule.EVEN_ODD
+    with pytest.raises(TypeError):
+        context.set_fill_rule(object())
 
 
 def test_set_font_face(context):
@@ -268,12 +323,58 @@ def test_set_font_face(context):
     ff = context.get_font_face()
     context.set_font_face(ff)
     assert context.get_font_face() == ff
+    with pytest.raises(TypeError):
+        context.set_font_face(object())
 
 
 def test_set_font_matrix(context):
     m = cairo.Matrix()
     context.set_font_matrix(m)
     assert context.get_font_matrix() == m
+    with pytest.raises(TypeError):
+        context.set_font_matrix(object())
+
+
+def test_set_line_cap(context):
+    context.set_line_cap(cairo.LineCap.SQUARE)
+    assert context.get_line_cap() == cairo.LineCap.SQUARE
+    with pytest.raises(TypeError):
+        context.set_line_cap(object())
+
+
+def test_set_line_join(context):
+    context.set_line_join(cairo.LineJoin.BEVEL)
+    assert context.get_line_join() == cairo.LineJoin.BEVEL
+    with pytest.raises(TypeError):
+        context.set_line_join(object())
+
+
+def test_set_line_width(context):
+    context.set_line_width(42)
+    assert context.get_line_width() == 42
+    with pytest.raises(TypeError):
+        context.set_line_width(object())
+
+
+def test_set_matrix(context):
+    m = cairo.Matrix()
+    context.set_matrix(m)
+    assert context.get_matrix() == m
+    with pytest.raises(TypeError):
+        context.set_matrix(object())
+
+
+def test_set_miter_limit(context):
+    context.set_miter_limit(42)
+    assert context.get_miter_limit() == 42
+    with pytest.raises(TypeError):
+        context.set_miter_limit(object())
+
+
+def test_set_scaled_font(context):
+    context.set_scaled_font(context.get_scaled_font())
+    with pytest.raises(TypeError):
+        context.set_miter_limit(object())
 
 
 def test_set_font_options(context):
@@ -287,6 +388,100 @@ def test_set_font_size(context):
         context.set_font_size(object())
 
 
+def test_set_source(context):
+    p = cairo.SolidPattern(0, 0, 0)
+    context.set_source(p)
+    assert context.get_source() == p
+    with pytest.raises(TypeError):
+        context.set_source(object())
+
+
+def test_set_source_rgb(context):
+    with pytest.raises(TypeError):
+        context.set_source_rgb(1, 1, object())
+
+
+def test_get_source_rgba(context):
+    context.set_source_rgba(1, 1, 1)
+    assert context.get_source().get_rgba() == (1, 1, 1, 1)
+    context.set_source_rgba(1, 1, 1, 0.5)
+    assert context.get_source().get_rgba() == (1, 1, 1, 0.5)
+    with pytest.raises(TypeError):
+        context.set_source_rgb(1, 1, object())
+
+
+def test_set_source_surface(context):
+    with pytest.raises(TypeError):
+        context.set_source_surface(object())
+
+
+def test_set_tolerance(context):
+    context.set_tolerance(42)
+    assert context.get_tolerance() == 42
+    with pytest.raises(TypeError):
+        context.set_tolerance(object())
+
+
+def test_show_glyphs(context):
+    with pytest.raises(TypeError):
+        context.show_glyphs()
+
+    with pytest.raises(TypeError):
+        context.show_glyphs(object())
+
+    context.show_glyphs([], 0)
+
+
+def test_show_text(context):
+    with pytest.raises(TypeError):
+        context.show_text()
+
+
+def test_stroke_extents(context):
+    assert context.stroke_extents() == (0.0, 0.0, 0.0, 0.0)
+
+
+def test_text_extents(context):
+    with pytest.raises(TypeError):
+        context.text_extents()
+
+
+def test_text_path(context):
+    context.text_path("foo")
+    with pytest.raises(TypeError):
+        context.text_path(object())
+
+
+def test_transform(context):
+    context.transform(cairo.Matrix())
+    with pytest.raises(TypeError):
+        context.transform(object())
+
+
+def test_translate(context):
+    context.translate(0.5, 0.5)
+    with pytest.raises(TypeError):
+        context.translate(0.5, object())
+
+
+def test_user_to_device(context):
+    assert context.user_to_device(0, 0) == (0, 0)
+    with pytest.raises(TypeError):
+        context.user_to_device(0, object())
+
+
+def test_user_to_device_distance(context):
+    assert context.user_to_device_distance(0, 0) == (0, 0)
+    with pytest.raises(TypeError):
+        context.user_to_device_distance(0, object())
+
+def test_context(context):
+    with pytest.raises(TypeError):
+        cairo.Context(None)
+
+    assert not context == object()
+
+
 def test_simple(context):
     context.clip_preserve()
     context.copy_page()
@@ -296,6 +491,8 @@ def test_simple(context):
     context.font_extents()
     context.identity_matrix()
     context.new_sub_path()
+    context.show_page()
+    context.stroke_preserve()
 
     assert context.get_dash_count() == 0
     assert isinstance(context.get_font_matrix(), cairo.Matrix)
