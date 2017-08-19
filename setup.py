2aeaa3a70a072cb3dd83f7016555fcff68432ec1
Christoph Reiter <reiter.christoph@gmail.com>
2017-08-19 11:42:17 +0200

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import errno
 from distutils.core import Extension, setup, Command, Distribution
 
 
-PYCAIRO_VERSION = '1.15.1'
+PYCAIRO_VERSION = '1.15.2'
 CAIRO_VERSION_REQUIRED = '1.13.1'
 XPYB_VERSION_REQUIRED = '1.3'
 
