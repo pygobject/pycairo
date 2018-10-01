ec7006268f40dfadfee7b8f12e43a48f7499a582
Christoph Reiter <reiter.christoph@gmail.com>
2018-10-02 00:51:09 +0200

    ci: make ci fail if the msvc build fails. Fixes #122

--- .appveyor/msvc.bat
+++ .appveyor/msvc.bat
@@ -16,10 +16,14 @@ set PYTHON=C:\%PYTHON_ROOT%\python.exe
 ::~ SET DISTUTILS_USE_SDK=1
 ::~ SET MSSdk=1
 
-%PYTHON% -m pip install --upgrade setuptools
-%PYTHON% -m pip install --upgrade pytest hypothesis coverage codecov
-copy cairo-windows-%CAIRO_VER%\lib\%MSVC_PLATFORM%\cairo.dll cairo
+%PYTHON% -m pip install --upgrade setuptools || goto :error
+%PYTHON% -m pip install --upgrade pytest hypothesis coverage codecov || goto :error
+copy cairo-windows-%CAIRO_VER%\lib\%MSVC_PLATFORM%\cairo.dll cairo || goto :error
 set CL=/WX
-%PYTHON% -m coverage run --branch setup.py test
-%PYTHON% -m coverage xml
-%PYTHON% -m codecov -f coverage.xml
+%PYTHON% -m coverage run --branch setup.py test || goto :error
+%PYTHON% -m coverage xml || goto :error
+%PYTHON% -m codecov --required -f coverage.xml || goto :error
+
+goto :EOF
+:error
+exit /b 1
