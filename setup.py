02ee7015c591bea1d3bdc9f630f295caa9dbf7a1
Stuart Axon <stuaxo2@yahoo.com>
2019-12-02 14:25:02 +0000

    Update setup.py
    Check -Wno-unused-command-line-argument in the correct place.

--- setup.py
+++ setup.py
@@ -92,6 +92,11 @@ def filter_compiler_arguments(compiler, args):
         extra += ['-Werror=unknown-warning-option']
     if check_argument(compiler, '-Werror=unused-command-line-argument'):
         extra += ['-Werror=unused-command-line-argument']
+    
+    # silence clang for unused gcc CFLAGS added by Debian
+    if check_argument(compiler, '-Wno-unused-command-line-argument'):
+        args += ["-Wno-unused-command-line-argument"]
+
 
     # first try to remove all arguments contained in the error message
     supported = list(args)
@@ -145,11 +150,6 @@ def add_ext_cflags(ext, compiler):
         "-Wno-missing-field-initializers",
         "-Wno-unused-parameter",
     ]
-
-    # silence clang for unused gcc CFLAGS added by Debian
-    if os.name != 'nt':
-        # hack to avoid Msys version of GCC that doesn't know -no-unused-command-line-argument
-        args += ["-Wno-unused-command-line-argument"]
 
     args += [
         "-fno-strict-aliasing",
