8d0b05c545af7814dfba0b9f09e68cd4cc9d00c7
Christoph Reiter <reiter.christoph@gmail.com>
2020-09-11 11:07:45 +0200

    CI: add Python 3.9
    and clean up the classifier list

--- setup.py
+++ setup.py
@@ -600,9 +600,6 @@ def main():
         classifiers=[
             'Operating System :: OS Independent',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: Implementation :: CPython',
             'Programming Language :: Python :: Implementation :: PyPy',
             ('License :: OSI Approved :: '
