03a5f54ee378908339a1824382d4b5e2a12c287d
Naveen M K <naveen521kk@gmail.com>
2022-09-17 18:05:12 +0530

    Skip the cmodule if the module isn't built. (useful when testing the wheels)

--- tests/test_cmod.py
+++ tests/test_cmod.py
@@ -10,6 +10,7 @@ try:
 except ImportError:
     pytest.skip("cmod not built", allow_module_level=True)
 
+
 def test_foo():
     surface = cmod.create_image_surface()
     assert isinstance(surface, cairo.ImageSurface)
