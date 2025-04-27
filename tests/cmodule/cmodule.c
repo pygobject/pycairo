41705e85922ffd3bc6f64e05e3985304582b4918
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-27 19:03:35 +0200

    Port to multi phase init
    https://docs.python.org/3/c-api/module.html#multi-phase-initialization
    
    Fixes #260

--- tests/cmodule/cmodule.c
+++ tests/cmodule/cmodule.c
@@ -2,36 +2,40 @@
 #include <py3cairo.h>
 #include "cmodulelib.h"
 
-static PyMethodDef CModMethods[] = {
-    {"create_image_surface", create_image_surface, METH_NOARGS, NULL},
-    {NULL, NULL, 0, NULL}
-};
-
 #ifdef __GNUC__
 #define PYCAIRO_MODINIT_FUNC __attribute__((visibility("default"))) PyMODINIT_FUNC
 #else
 #define PYCAIRO_MODINIT_FUNC PyMODINIT_FUNC
 #endif
 
+static PyMethodDef cmod_functions[] = {
+    {"create_image_surface", create_image_surface, METH_NOARGS, NULL},
+    {NULL, NULL, 0, NULL}};
+
+static int exec_cmod(PyObject *m)
+{
+    if (import_cairo() < 0) {
+        return -1;
+    }
+    return 0;
+}
+
+static PyModuleDef_Slot cmod_slots[] = {
+    {Py_mod_exec, exec_cmod},
+    {0, NULL}};
+
 static struct PyModuleDef cmod_module = {
     PyModuleDef_HEAD_INIT,
     "cmod",
     NULL,
-    -1,
-    CModMethods,
-};
+    0,
+    cmod_functions,
+    cmod_slots,
+    NULL,
+    NULL,
+    NULL};
 
-PYCAIRO_MODINIT_FUNC
-PyInit_cmod (void)
+PYCAIRO_MODINIT_FUNC PyInit_cmod(void)
 {
-    PyObject *m;
-
-    if (import_cairo () < 0)
-        return NULL;
-
-    m = PyModule_Create (&cmod_module);
-    if (m == NULL)
-        return NULL;
-
-    return m;
+    return PyModuleDef_Init(&cmod_module);
 }
